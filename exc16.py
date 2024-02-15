bdb39123b6d1afc04504800c4c67328c33a9e041
Rogerszx <104950900+Rogerszx@users.noreply.github.com>
2024-02-15 10:00:10 -0300

    Add files via upload

---
+++ exc16.py
@@ -0,0 +1,23 @@
+# Exercício 6 - Crie uma variável chamada contador = 0. Enquanto counter for menor que 100, imprima os valores na tela,
+# mas quando for encontrado o valor 23, interrompa a execução do programa
+
+contador = 0
+
+while contador < 100:
+    print(contador)
+    contador += 1
+    if contador == 23:
+        break
+
+
+#####
+    
+valor = 0 
+
+while valor < 100:
+    if valor == 4:
+        break
+else:
+    pass
+print(valor)
+valor = valor + 1
---
+++ exc7.py
@@ -0,0 +1,8 @@
+dic = {
+        'nome':'Roger',
+        'sobrenome':'Gonçalves',
+        'idade':21
+}
+
+dic['último sobrenome'] = 'dos Santos'
+print(dic)
